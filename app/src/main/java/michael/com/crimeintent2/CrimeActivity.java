db584eda7da8c02a3c60e652a94620b799b0e979
Michael <michaelposada@mail.adelphi.edu>
2018-09-23 19:41:14 -0400

    Home work 2

---
+++ app/src/main/java/michael/com/crimeintent2/CrimeActivity.java
@@ -0,0 +1,16 @@
+package michael.com.crimeintent2;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends SingleFragmentActivity{
+    @Override
+    protected Fragment createFragment()
+    {
+        return new CrimeFragment();
+    }
+
+
+}
